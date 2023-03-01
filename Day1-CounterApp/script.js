1c20a625b5820f46dd27b5f0a6fa9d9e9f5ee4b3
LILIANA VELAZQUEZ <lilianavelazquez@LILIANAs-MacBook-Pro.local>
2023-02-28 16:37:45 -0800

    refactored Javascript file

--- Day1-CounterApp/script.js
+++ Day1-CounterApp/script.js
@@ -1,14 +1,30 @@
 const count = document.querySelector(".count");
-const add = document.querySelector(".add");
-const resetCount = document.querySelector(".reset");
-const sub = document.querySelector(".subtract");
+const buttons = document.querySelector(".buttons");
 
-add.addEventListener("click", () => {
-  count.innerHTML++;
+buttons.addEventListener("click", (e) => {
+  if (e.target.classList.contains("add")) {
+    console.log("Add");
+    count.innerHTML++;
+    setColor();
+  }
+  if (e.target.classList.contains("reset")) {
+    console.log("Reset");
+    count.innerHTML = 0;
+    setColor();
+  }
+  if (e.target.classList.contains("subtract")) {
+    console.log("Subtract");
+    count.innerHTML--;
+    setColor();
+  }
 });
-sub.addEventListener("click", () => {
-  count.innerHTML--;
-});
-resetCount.addEventListener("click", () => {
-  count.innerHTML = 0;
-});
+
+function setColor() {
+  if (count.innerHTML > 0) {
+    count.style.color = "turquoise";
+  } else if (count.innerHTML < 0) {
+    count.style.color = "hotpink";
+  } else {
+    count.style.color = "gray";
+  }
+}
